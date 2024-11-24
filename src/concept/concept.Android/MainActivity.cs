b88c62f6bfc8bbb4c3c20e25e4ae75e750820c83
keyldev <wudgexyz@gmail.com>
2024-11-25 00:06:46 +0300

    feat: concept instead

---
+++ src/concept/concept.Android/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.ReactiveUI;
+
+namespace concept.Android
+{
+    [Activity(
+        Label = "concept.Android",
+        Theme = "@style/MyTheme.NoActionBar",
+        Icon = "@drawable/icon",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+    public class MainActivity : AvaloniaMainActivity<App>
+    {
+        protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+        {
+            return base.CustomizeAppBuilder(builder)
+                .WithInterFont()
+                .UseReactiveUI();
+        }
+    }
+}
---
+++ src/concept/concept/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace concept.ViewModels
+{
+    public abstract class ViewModelBase : ReactiveObject
+    {
+    }
+}
